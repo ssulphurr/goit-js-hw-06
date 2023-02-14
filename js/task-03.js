ed3a1d8516986f7c7ecffefe6ea7f453f3ba6960
ssulphurr <ssulphurr@gmail.com>
2023-02-14 23:10:05 +0200

    adds task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,23 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// Напиши скрипт для створення галереї зображень на підставі масиву даних. HTML містить список ul.gallery.
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. 
+// Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+
+const galleyString = images
+.map((image) => `<li> <img src = "${image.url}" alt = "${image.alt}"> </li>`)
+.join('');
+
+// console.log(galleyString)
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML("afterbegin", galleyString)
+
+galleryEl.classList.add("gallery_modified")
